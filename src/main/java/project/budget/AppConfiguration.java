fe5917fb1ed6e16b3b39f88a65b657f241559984
vsoufflet <valentin.soufflet@gmail.com>
2020-04-14 15:11:54 +0200

    Résolution du conflit d'auto-configuration de spring: @SpringConfiguration -> @Configuration

---
+++ src/main/java/project/budget/AppConfiguration.java
@@ -0,0 +1,9 @@
+package project.budget;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan
+@Configuration
+public class AppConfiguration {
+}
--- src/main/java/project/budget/Configuration.java
+++
@@ -1,9 +0,0 @@
-package project.budget;
-
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-
-@ComponentScan
-@SpringBootConfiguration
-public class Configuration {
-}
